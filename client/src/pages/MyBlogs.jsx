3cd4b257b07349ab2e8e423cc3216e2339d9109b
imRahulAgarwal <imagarwal05@gmail.com>
2024-03-23 18:03:39 +0530

    Changes in MyBlogs.jsx

--- client/src/pages/MyBlogs.jsx
+++ client/src/pages/MyBlogs.jsx
@@ -17,7 +17,9 @@ const MyBlogs = () => {
             <h1 className="blog-section-title section-title">All Blogs</h1>
             <div
                 className={`grid mt-5 mb-10 ${
-                    blogs.length ? "lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6" : ""
+                    blogs && blogs.length
+                        ? "lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"
+                        : ""
                 }`}>
                 {blogs && blogs.length ? (
                     blogs.map((blog) => (
