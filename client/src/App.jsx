bab4c6fb80b8cb33d634e237dde44bfe2dbfd855
imRahulAgarwal <imagarwal05@gmail.com>
2024-03-23 19:12:01 +0530

    Changes in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Footer, Header } from "./import";
 import { Outlet } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
