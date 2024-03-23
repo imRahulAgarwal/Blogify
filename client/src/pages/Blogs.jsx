464d4835735933a4dab785ad15f2a5e1f79ff72f
imRahulAgarwal <imagarwal05@gmail.com>
2024-03-23 17:46:44 +0530

    Changes in Blogs.jsx

--- client/src/pages/Blogs.jsx
+++ client/src/pages/Blogs.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Card, Container, Pagination } from "../import";
-import { PuffLoader, ScaleLoader } from "react-spinners";
 
 const Blogs = () => {
     const { blogs } = useSelector((state) => state.blog);
@@ -9,30 +8,22 @@ const Blogs = () => {
     return (
         <div className="min-h-screen flex">
             <Container classes="flex flex-col my-auto">
-                {!blogs ? (
-                    <ScaleLoader color="#000" className="mx-auto my-auto" />
-                ) : (
-                    <div className="mt-40">
-                        <h1 className="blog-section-title section-title">All Blogs</h1>
-                        <div
-                            className={`grid mt-5 mb-10 ${
-                                blogs.length
-                                    ? "lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6"
-                                    : ""
-                            }`}>
-                            {blogs && blogs.length ? (
-                                blogs.map((blog) => (
-                                    <Card blog={blog} key={blog._id} to={blog._id} />
-                                ))
-                            ) : (
-                                <h1 className="not-found max-sm:text-2xl text-3xl">
-                                    Blogs not listed yet
-                                </h1>
-                            )}
-                        </div>
-                        {blogs && blogs.length ? <Pagination /> : null}
+                <div className="mt-40">
+                    <h1 className="blog-section-title section-title">All Blogs</h1>
+                    <div
+                        className={`grid mt-5 mb-10 ${
+                            blogs.length ? "lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-6" : ""
+                        }`}>
+                        {blogs && blogs.length ? (
+                            blogs.map((blog) => <Card blog={blog} key={blog._id} to={blog._id} />)
+                        ) : (
+                            <h1 className="not-found max-sm:text-2xl text-3xl">
+                                Blogs not listed yet
+                            </h1>
+                        )}
                     </div>
-                )}
+                    {blogs && blogs.length ? <Pagination /> : null}
+                </div>
             </Container>
         </div>
     );
